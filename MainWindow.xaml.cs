91c18f73d5b54de0c98a2b6769507579ee2d7de1
Mantie Reid <mantiereid2@gmail.com>
2019-05-24 17:48:37 -0400

    Added Win function for winning, ties and looses on the rock option.  Did the same for paper option as well. Next, Just need to do the same for the Scissor option for the cpu.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -107,22 +107,93 @@ namespace Rock_paper_scissors
 
       Console.WriteLine($"choice picked is " + choicepicked); //writes to the console to show what the choice is
 
-      if (choicepicked.Equals("rock")) //if choice picked equals rock then.....
+      if (choicepicked.Equals("rock")) //if choice picked equals rock then load rock image for cpu. 
 
       {
-        Player2_Option_Chosen.Source = new BitmapImage(new Uri(" Mickey Rock for the right side.jpg", UriKind.Relative)); //loads the rock image. //changes the rock image to 
+        Player2_Option_Chosen.Source = new BitmapImage(new Uri(" Mickey Rock for the right side.jpg", UriKind.Relative)); //loads the rock image. 
       }
+
+
+      if (choicepicked.Equals("paper")) //if choice picked equals rock then load paper image for cpu. 
+
+      {
+        Player2_Option_Chosen.Source = new BitmapImage(new Uri(" PAPER Proper right.png", UriKind.Relative)); //loads the Paper image.
+
+      }
+
+      if (choicepicked.Equals("paper") && Scissior_Option.IsChecked == true) //if choice picked equals paper and the player chose scissors then the player wins
+
+      {
+        //player wins. Increase the number of wins.
+        numberofwins += 1; // increase number of wins by one 
+       Console.WriteLine("number of wins is " + numberofwins); //writes the number of wins in the console. For Testing and debugging. 
+
+      }
+
+
+      if (choicepicked.Equals("paper") && Rock_Option.IsChecked  == true) //if choice picked equals paper and the player chose rock then the player looses
+
+      {
+        //player loses. Increse the number of loses
+        numberoflosses += 1; // increase number of wins by one
+        Console.WriteLine("the number of losses is" + numberoflosses); //prints the number of losses to console.
+        
+
+
+      }
+
+
+      if (choicepicked.Equals("paper") && Paper_Option.IsChecked == true) //if choice picked equals paper and the player chose paper then a tie is declared. 
+
+      {
+        //Tie is declared. Increase the number of ties. 
+        numberofties += 1; // increase the number of ties. 
+        Console.WriteLine("the number of ties is" + numberofties); //prints the number of ties to console. 
+
+
+
+      }
+
+
 
 
       if (choicepicked.Equals("rock") && Paper_Option.IsChecked == true) // if paper option is made by and rock is chosen by the cpu then..
 
       {
+        //player wins. Increase the  number of wins. 
         numberofwins += 1; // increase number of wins by one 
 
         Console.WriteLine("number of wins is " + numberofwins); //writes the number of wins in the console. For Testing and debugging. 
 
       }
-      Number_of_wins_textblock.Text = "Wins" + numberofwins; // puts the number of wins in the textblock. 
+
+
+      if (choicepicked.Equals("rock") && Scissior_Option.IsChecked == true) // if rock is chosen by the cpu, player chooses scissior then the player looses
+
+      {
+        //player looses, increase the number of looses
+        numberoflosses += 1; // increase number of looses by one  
+
+        Console.WriteLine("number of looses is " + numberoflosses); //writes the number of looses in the console. For Testing and debugging. 
+
+      }
+
+
+      if (choicepicked.Equals("rock") && Rock_Option.IsChecked == true) // if rock is chosen by the cpu, player chooses rock then a tie is declared
+
+      {
+        // a Tie is declared. Incrase the number of ties
+        numberofties += 1; // increase number of ties 
+
+        Console.WriteLine("number of ties is " + numberofties); //writes the number of ties in the console. For Testing and debugging. 
+
+      }
+
+
+      Number_of_wins_textblock.Text = "Wins " + numberofwins; // puts the number of wins in the textblock.
+      Number_of_Losses_textblock.Text = "Loses " + numberoflosses; // puts the number of losses in the textblock.
+      Number_of_Ties_textblock.Text = "Ties " + numberofties;
+      
 
 
     }
