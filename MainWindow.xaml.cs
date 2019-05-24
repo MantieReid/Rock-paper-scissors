1cc52320022d76556d7b8d66c4f9c3c70f71ffa2
Mantie Reid <mantiereid2@gmail.com>
2019-05-24 16:55:12 -0400

    Added counter for number of wins.  Added sound to played when the player selects Rock, paper, or scissior.  Added function to allow the CPu to pick a choice randomnly. Added function to Determine if the player wins for the choice paper.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,7 +27,15 @@ namespace Rock_paper_scissors
     public MainWindow()
     {
       InitializeComponent();
+
+
+     
     }
+
+
+    int numberofwins = 0; // the number of wins
+    int numberoflosses = 0; // the number of losses
+    int numberofties = 0;  // the number of ties
 
 
     private void RadioButton_Checked(object sender, RoutedEventArgs e) ///Rock Option
@@ -42,6 +50,8 @@ namespace Rock_paper_scissors
         player.Play(); // plays the file
 
 
+      
+
 
       
 
@@ -50,20 +60,84 @@ namespace Rock_paper_scissors
 
     private void Paper_Option_Checked(object sender, RoutedEventArgs e) // paper option
     {
-      //Player1_Option_Chosen.Source = new BitmapImage(new Uri(@"C:\Users\gunzs\source\repos\Rock–paper–scissors\PAPER Proper left.png"));
-      Player1_Option_Chosen.Source = new BitmapImage(new Uri("PAPER Proper left.png", UriKind.Relative));
+      Player1_Option_Chosen.Source = new BitmapImage(new Uri("PAPER Proper left.png", UriKind.Relative)); // loads the paper image 
+
+      SoundPlayer player = new SoundPlayer(); // creates a new sound player
+      player.Stream = Properties.Resources.Paper_Sound_Wav; // plays the paper sound file. 
+      player.Load(); // loads the file 
+      player.Play(); // plays the file
+      
 
     }
 
     private void Scissior_Option_Checked(object sender, RoutedEventArgs e) // scissior option checked
     {
-      Player1_Option_Chosen.Source = new BitmapImage(new Uri(@"C:\Users\gunzs\source\repos\Rock–paper–scissors\Scissors.jpg"));
+      Player1_Option_Chosen.Source = new BitmapImage(new Uri("Scissors.jpg", UriKind.Relative)); //loads the scissors image.
 
+      SoundPlayer player = new SoundPlayer(); // creates a new sound player
+      player.Stream = Properties.Resources.Scissors; // plays the paper sound file. 
+      player.Load(); // loads the file 
+      player.Play(); // plays the file
+
+      
     }
 
-    private void Button_Click(object sender, RoutedEventArgs e) //run game button
+
+    public static int NumMembers { get; protected set; }
+
+
+    
+
+    public void Button_Click(object sender, RoutedEventArgs e) //run game button
     {
+      //array of choices to pick for the cpu.
+
+      var choices  =  new List<string> { "rock", "paper", "scissor" };
+
+      //0 = rock, 1 = paper. 3 = scissor. 
+
+      //create a randomn object
+
+
+      
+      Random random = new Random();
+
+      int index = random.Next(choices.Count); // randomly pick a choice. The CPU picks a random choice.
+      String choicepicked = choices[index]; // choice is stored in the string. 
+
+      Console.WriteLine($"choice picked is " + choicepicked); //writes to the console to show what the choice is
+
+      if (choicepicked.Equals("rock")) //if choice picked equals rock then.....
+
+      {
+        Player2_Option_Chosen.Source = new BitmapImage(new Uri(" Mickey Rock for the right side.jpg", UriKind.Relative)); //loads the rock image. //changes the rock image to 
+      }
+
+
+      if (choicepicked.Equals("rock") && Paper_Option.IsChecked == true) // if paper option is made by and rock is chosen by the cpu then..
+
+      {
+        numberofwins += 1; // increase number of wins by one 
+
+        Console.WriteLine("number of wins is " + numberofwins); //writes the number of wins in the console. For Testing and debugging. 
+
+      }
+      Number_of_wins_textblock.Text = "Wins" + numberofwins; // puts the number of wins in the textblock. 
+
 
     }
-  }
+
+
+    //choices.RemoveAt(index);
+    ///Console.WriteLine($"randomnly selected choice is {choices[index]}");
+
+
+
+
+
+
+     
+    }
 }
+
+
