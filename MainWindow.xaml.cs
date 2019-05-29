4e5446f973d53e2bc749b6f7a6b6803d530c5797
Mantie Reid <mantiereid2@gmail.com>
2019-05-29 18:19:15 -0400

    Added Fody Costrua to have files embed in the exe.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,30 +1,10 @@
+using MahApps.Metro.Controls;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.IO;
-using System.Linq;
 using System.Media;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
-using Image = System.Drawing.Image;
-
-
-
-
-using MahApps.Metro.Controls;
-using System.Globalization;
-using Color = System.Windows.Media.Color;
-using ColorConverter = System.Windows.Media.ColorConverter;
 
 namespace Rock_paper_scissors
 {
@@ -79,14 +59,14 @@ namespace Rock_paper_scissors
 
       SoundPlayer player = new SoundPlayer(); // creates a new sound player
       player.Stream = Properties.Resources.Falling_Rock_Sound; // plays the rock sound file. 
-        player.Load(); // loads the file 
-        player.Play(); // plays the file
+      player.Load(); // loads the file 
+      player.Play(); // plays the file
 
 
-      
 
 
-      
+
+
 
 
     }
@@ -99,7 +79,7 @@ namespace Rock_paper_scissors
       player.Stream = Properties.Resources.Paper_Soundfinal; // plays the paper sound file. 
       player.Load(); // loads the file 
       player.Play(); // plays the file
-      
+
 
     }
 
@@ -112,7 +92,7 @@ namespace Rock_paper_scissors
       player.Load(); // loads the file 
       player.Play(); // plays the file
 
-      
+
     }
 
 
@@ -126,7 +106,7 @@ namespace Rock_paper_scissors
     {
       //array of choices to pick for the cpu.
 
-      var choices  =  new List<string> { "rock", "paper", "scissor" };
+      var choices = new List<string> { "rock", "paper", "scissor" };
 
       //0 = rock, 1 = paper. 3 = scissor. 
 
@@ -139,7 +119,7 @@ namespace Rock_paper_scissors
       Console.WriteLine($"choice picked by the cpu is " + choicepicked); //writes to the console to show what the choice is for the cpu.
 
 
-      
+
       if (choicepicked.Equals("rock")) //if choice picked equals rock then load rock image for cpu. 
 
       {
@@ -159,15 +139,15 @@ namespace Rock_paper_scissors
       {
         //player wins. Increase the number of wins.
         numberofwins += 1; // increase number of wins by one 
-       Console.WriteLine("number of wins is " + numberofwins); //writes the number of wins in the console. For Testing and debugging.
+        Console.WriteLine("number of wins is " + numberofwins); //writes the number of wins in the console. For Testing and debugging.
 
-        
+
 
         numberoflossesCPU += 1; // increase the number of looses for the cpu
         Console.WriteLine("number of losses for the cpu is " + numberoflossesCPU);
 
 
-       /// Color color2 = (Color)ColorConverter.ConvertFromString("#FF00FF23"); screw it
+        /// Color color2 = (Color)ColorConverter.ConvertFromString("#FF00FF23"); screw it
 
 
         Game_Status_Textblock.Text = "VICTORY"; //change game status to victory since player won. 
@@ -187,7 +167,7 @@ namespace Rock_paper_scissors
       }
 
 
-      if (choicepicked.Equals("paper") && Rock_Option.IsChecked  == true) //if choice picked equals paper and the player chose rock then the player looses
+      if (choicepicked.Equals("paper") && Rock_Option.IsChecked == true) //if choice picked equals paper and the player chose rock then the player looses
 
       {
         //player loses. Increse the number of loses
@@ -299,7 +279,7 @@ namespace Rock_paper_scissors
 
       }
 
-     
+
 
       if (choicepicked.Equals("rock") && Rock_Option.IsChecked == true) // if rock is chosen by the cpu, player chooses rock then a tie is declared
 
@@ -329,7 +309,7 @@ namespace Rock_paper_scissors
 
 
 
-      if (choicepicked.Equals("scissor") ) // if scissior is chosen by the cpu, load the scissior image
+      if (choicepicked.Equals("scissor")) // if scissior is chosen by the cpu, load the scissior image
 
       {
 
@@ -423,7 +403,7 @@ namespace Rock_paper_scissors
 
         Game_Status_Textblock.Text = "SUCCESS"; //change game status to victory since player won. 
         Game_Status_Textblock.Foreground = System.Windows.Media.Brushes.Gold; // change text to gold. 
-        
+
 
         SoundPlayer player = new SoundPlayer(); // creates a new sound player
         player.Stream = Properties.Resources.snd_se_narration_Success; // plays the sucess soud file
@@ -443,7 +423,7 @@ namespace Rock_paper_scissors
       Number_of_wins_textblock.Text = "Wins " + numberofwins; // puts the number of wins in the textblock.
       Number_of_Losses_textblock.Text = "Loses " + numberoflosses; // puts the number of losses in the textblock.
       Number_of_Ties_textblock.Text = "Ties " + numberofties; // the number of ties is put into the textblock. 
-      Number_of_Losses_CPU_textblock.Text = "Loses " +  numberoflossesCPU; // the number of losses for the cpu is put into the textblock. 
+      Number_of_Losses_CPU_textblock.Text = "Loses " + numberoflossesCPU; // the number of losses for the cpu is put into the textblock. 
       Number_of_Ties_CPU_textblock.Text = "Ties " + numberoftiesCPU; //the number of ties for the cpu is put into the textblock.
       Number_of_wins_CPU_textblock.Text = "Wins " + numberofwinsCPU; //the number of wins for the cpu is put into the textblock. 
 
@@ -455,24 +435,24 @@ namespace Rock_paper_scissors
     /// </summary>
     /// <param name="sender"></param>
     /// <param name="e"></param>
-   // private void playsound(object sender, RoutedEventArgs e)
-  //  {
-   //   SoundPlayer player = new SoundPlayer(); // creates a new sound player
-   //   player.Stream = Properties.Resources.snd_se_narration_Player_01; // plays the paper sound file. 
-   //   player.Load(); // loads the file 
-   //   player.Play(); // plays the file
+    // private void playsound(object sender, RoutedEventArgs e)
+    //  {
+    //   SoundPlayer player = new SoundPlayer(); // creates a new sound player
+    //   player.Stream = Properties.Resources.snd_se_narration_Player_01; // plays the paper sound file. 
+    //   player.Load(); // loads the file 
+    //   player.Play(); // plays the file
 
-   //   System.Threading.Thread.Sleep(3000);
+    //   System.Threading.Thread.Sleep(3000);
 
     //  SoundPlayer player2 = new SoundPlayer(); // creates a new sound player
-     // player2.Stream = Properties.Resources.snd_se_narration_Player_01; // plays the paper sound file. 
-     // player2.Load(); // loads the file 
-     // player2.Play(); // plays the file
+    // player2.Stream = Properties.Resources.snd_se_narration_Player_01; // plays the paper sound file. 
+    // player2.Load(); // loads the file 
+    // player2.Play(); // plays the file
 
 
-   // }
+    // }
 
-    
+
 
     private void playsound(object sender, RoutedEventArgs e) //plays when the textblock loads
     {
@@ -487,7 +467,7 @@ namespace Rock_paper_scissors
       player2.Stream = Properties.Resources.snd_se_narration_Go; // plays the go sound
       player2.Load(); // loads the file 
       player2.Play(); // plays the file
-     
+
 
     }
 
@@ -497,41 +477,41 @@ namespace Rock_paper_scissors
 
       window1.Show();
 
-        }
-
-
-
-
-        //TODO: Load the sound effects when the player wins, looses. DONE. 
-        //TODO: If you can, find a sound effect for a  game tie. (Myabe a audience shock effect might be  a good idea).  DONE
-
-
-        //TODO: Add The game Status Text when the player looses, Wins, or gets a tie. DONE
-
-        //TODO: Add Wins, Ties and losses counter for the CPU. DONE.
-        //TODO: Make the Wins, Ties, and losses Counter Functionsl for the cpu. DONE
-
-        //TODO: Add the sound Effect game for when a tie is declared.  DONE
-
-        //TODO:get rid of the old images we do not use. Try to orgnaize them into a folder if you can. DONE
-
-        //TODO: Get rid of Sound files we are not using.
-        //TODO: Add a About menu or a label with your name on it.
-        //TODO: Organize the soud files into one folder.
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
+
+
+
+
+    //TODO: Load the sound effects when the player wins, looses. DONE. 
+    //TODO: If you can, find a sound effect for a  game tie. (Myabe a audience shock effect might be  a good idea).  DONE
+
+
+    //TODO: Add The game Status Text when the player looses, Wins, or gets a tie. DONE
+
+    //TODO: Add Wins, Ties and losses counter for the CPU. DONE.
+    //TODO: Make the Wins, Ties, and losses Counter Functionsl for the cpu. DONE
+
+    //TODO: Add the sound Effect game for when a tie is declared.  DONE
+
+    //TODO:get rid of the old images we do not use. Try to orgnaize them into a folder if you can. DONE
+
+    //TODO: Get rid of Sound files we are not using.
+    //TODO: Add a About menu or a label with your name on it.
+    //TODO: Organize the soud files into one folder.
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
 }
 
 
--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -1,17 +1,8 @@
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Rock_paper_scissors
 {
@@ -24,10 +15,10 @@ namespace Rock_paper_scissors
     {
       InitializeComponent();
     }
-    
+
     private void RichTextBox_TextChanged(object sender, TextChangedEventArgs e)
     {
-      
+
     }
 
 
